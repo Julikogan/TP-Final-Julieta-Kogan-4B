357071d51eef377e4907c287090c50a3a74c0ff1
Julikogan <163456780+Julikogan@users.noreply.github.com>
2024-06-19 15:50:14 -0300

    fin

--- Assets/Array.cs
+++ Assets/Array.cs
@@ -1,21 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
 
 public class Array : MonoBehaviour
-
 {
-    public GameObject[] objetos;
+    public GameObject[] arrayObjetos;
+    private int idx = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            DeactivateAll();
+            arrayObjetos[idx].SetActive(true);
+            if (idx != arrayObjetos.Length - 1)
+            {
+                idx++;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            DeactivateAll();
+            arrayObjetos[idx].SetActive(true);
+            if (idx != 0)
+            {
+                idx--;
+            }
+        }
+    }
+
+    void DeactivateAll()
+    {
+        foreach (var objeto in arrayObjetos)
+        {
+            objeto.SetActive(false);
+        }
     }
 }
